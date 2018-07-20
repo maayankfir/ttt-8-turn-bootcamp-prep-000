aeedba0bdc3e194b9a9bc814a3ba132f491c073b
maayankfir <mayankfir@gmail.com>
2018-07-20 20:10:06 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,4 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+
 
 def turn(board)
   puts "Please enter 1-9:"
@@ -22,6 +16,10 @@ else
 end
 end
 
+def move(board, input_to_index, current_player = "X")
+  board[input_to_index] = current_player
+end
+
 def position_taken?(board, index)
 if board[index] == " " || board[index] == "" || board[index] == nil
   false
@@ -30,8 +28,10 @@ else
 end
 end
 
-def move(board, input_to_index, current_player = "X")
-  board[input_to_index] = current_player
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-
