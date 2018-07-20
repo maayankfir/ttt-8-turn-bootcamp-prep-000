173b1743d59d35403e6ed89d3577126c2700d2d2
maayankfir <mayankfir@gmail.com>
2018-07-20 20:22:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,19 +1,13 @@
-
-
-def turn(board)
-  puts "Please enter 1-9:"
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def input_to_index(user_input)
   user_input.to_i - 1
-end
-
-def valid_move?(board, index)
-if index.between?(0, 8) && position_taken?(board, index) == false
-  true
-else
-  nil
-end
 end
 
 def move(board, input_to_index, current_player = "X")
@@ -28,10 +22,26 @@ else
 end
 end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+def valid_move?(board, index)
+if index.between?(0, 8) && position_taken?(board, index) == false
+  true
+else
+  nil
 end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+
+end
+
+
+
+
+
+
+
+
+
+
